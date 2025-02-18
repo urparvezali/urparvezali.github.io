b41e82aedcea887f995da2a587e6638adfb1529f
Parvez Ali <urparvezali@gmail.com>
2025-02-18 16:02:56 +0600

    CF profile added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-	const [count, setCount] = useState(0)
+export const App = () => {
+	const [information, setInformation] = useState("Getting data...");
+	const [userinfo, setUserinfo] = useState(null);
+
+	useEffect(() => {
+		const fetch_data = async () => {
+			const res = await fetch("https://codeforces.com/api/user.info?handles=urparvezali");
+			if (!res.ok) {
+				setInformation("Can't fetch user info!!");
+				return;
+			}
+			setUserinfo((await res.json()).result[0]);
+			setInformation("Got data!");
+		}
+		fetch_data();
+	}, []);
 
 	return (
-
-		<button onClick={() => setCount((count) => count + 1)}>
-			count is {count}
-		</button>
-
-	)
-}
-
-export default App
+		<div>
+			{userinfo ? (
+				<div>
+					<img src={"https://media.licdn.com/dms/image/v2/D5603AQHwv3vQRLSXqw/profile-displayphoto-shrink_400_400/B56ZO5TgJfGcAk-/0/1733980719487?e=1745452800&v=beta&t=9E4Kd2_-dx0ffvsKGP2-gcsQ1IesZbf0Ob9YCIfJvYQ"} alt="Title Photo" width="200" />
+					<h2>{userinfo.handle}</h2>
+					<p>Name: {userinfo.firstName} {userinfo.lastName}</p>
+					<p>Country: {userinfo.country}</p>
+					<p>City: {userinfo.city}</p>
+					<p>Rank: {userinfo.rank} (Max: {userinfo.maxRank})</p>
+					<p>Rating: {userinfo.rating} (Max: {userinfo.maxRating})</p>
+					<p>Contribution: {userinfo.contribution}</p>
+					<p>Friends: {userinfo.friendOfCount}</p>
+					<p>Organization: {userinfo.organization}</p>
+				</div>
+			) : (
+				<p>Loading user info...</p>
+			)}
+		</div>
+	);
+};
